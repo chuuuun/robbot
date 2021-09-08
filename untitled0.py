2aec93ffdaf3ef333ceecbc6403d187d511e7e03
chuuuun <90257280+chuuuun@users.noreply.github.com>
2021-09-08 13:55:29 +0800

    k

--- untitled0.py
+++ untitled0.py
@@ -5,5 +5,11 @@ Created on Tue Sep  7 20:36:13 2021
 
 @author: chuuun
 """
+import discord
+from discord.ext import commands
 
-print('hello')
+robbot=commands.Bot(command_prefix='!')
+@robbot.event
+async def on_ready():
+    print('>>robbot is online<<')
+robbot.run('ODg0Nzc2NzczMDMwNjcwMzk2.YTdalQ.JWll25v2bORYV5glFV3aQWzchks')
